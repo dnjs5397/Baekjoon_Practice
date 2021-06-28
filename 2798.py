cb0b8aafc51c20d9ca8c7c748f19ff52fa2318e0
dnjs5397 <dnjs5397@naver.com>
2021-06-28 21:53:28 +0900

    Baekjoon_2798

--- 2798.py
+++ 2798.py
@@ -1,5 +1,5 @@
 # 블랙잭, 탐색 문제
-
+# 파이썬은 1초에 2천만까지의 연산 가능
 N, M = input().split(' ')
 N = int(N)
 M = int(M)
@@ -10,8 +10,8 @@ max = 0
 sum = 0
 
 for i in range(0, N):
-    for j in range(1, N):
-        for k in range(2, N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
             sum = card[i] + card[j] + card[k]
             if (sum >= max and sum <= 21):
                 max = sum
