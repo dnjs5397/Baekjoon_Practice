a68ec2d20d91dcd84a9c4b1eee465bf890e4855f
Seungwon <dnjs5397@naver.com>
2022-05-03 23:59:15 +0900

    practice

--- 21756.py
+++ 21756.py
@@ -1,6 +1,13 @@
 N = int(input())
 
-arr = [i for i in range(1,N+1)]
-print(arr)
-del(arr)   
-print(arr) 
+arr = [i+1 for i in range(N)]
+
+tmp = []
+
+while(len(arr) != 1):
+    for i in range(len(arr)):
+        if i%2:
+            tmp.append(arr[i])
+    arr = tmp
+    tmp = []
+print(arr[0])
