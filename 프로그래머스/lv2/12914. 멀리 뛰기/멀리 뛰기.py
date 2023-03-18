a6144185c0a139c9fdff4d915328d9074dd4aac0
Seungwon <63894186+dnjs5397@users.noreply.github.com>
2023-03-18 15:56:02 +0900

    Update 멀리 뛰기.py

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,24 +1,34 @@
-def factorial(num):
-    N = 1
-    while(num > 0):
-        N *= num
-        num -= 1
-    return N
+# def factorial(num):
+#     N = 1
+#     while(num > 0):
+#         N *= num
+#         num -= 1
+#     return N
 
+# def solution(n):
+#     ans = 0
+#     length = n//2
+
+#     for i in range(0, length+1):
+#         if i == 0:
+#             ans += 1
+#             continue
+#         N = i + (n-(i*2))
+#         R = (n-(i*2))
+#         if R == 0:
+#             ans += 1
+#             continue
+#         ans += factorial(N) // (factorial(N-R) * factorial(R))
+
+
+#     return ans%1234567
+
+# DP 유형의 대표 문제 -> 팩토리얼로도 풀 수 있지만 효율성을 위해 DP를 이해할 것
 def solution(n):
-    ans = 0
-    length = n//2
+    arr = [1] * 2 + [0] * n
+    
+    for i in range(2, n+1):
+        arr[i] = arr[i-1] + arr[i-2]
+    
+    return arr[n]%1234567
 
-    for i in range(0, length+1):
-        if i == 0:
-            ans += 1
-            continue
-        N = i + (n-(i*2))
-        R = (n-(i*2))
-        if R == 0:
-            ans += 1
-            continue
-        ans += factorial(N) // (factorial(N-R) * factorial(R))
-
-
-    return ans%1234567
