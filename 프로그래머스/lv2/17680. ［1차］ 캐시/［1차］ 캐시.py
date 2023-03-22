f0feb211b68c17020ad87d5cb64b28d5ce2d5e9b
Seungwon <63894186+dnjs5397@users.noreply.github.com>
2023-03-22 23:41:02 +0900

    Update ［1차］ 캐시.py (주석 추가)

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,20 +1,20 @@
 def solution(cacheSize, cities):
-    
+    # 캐시에 빈 공간이 없다면 바로 return
     if cacheSize == 0:
         return len(cities)*5
     
     answer = 0
     cache = []
     for i in cities:
-        i = i.lower()
-        if len(cache) < cacheSize:
+        i = i.lower() # 대소문자 구별 x
+        if len(cache) < cacheSize: #캐시 공간이 남을때
             if i in cache:
                 answer += 1
                 cache.append(i)
             else:
                 cache.append(i)
                 answer += 5
-        elif len(cache) == cacheSize:
+        elif len(cache) == cacheSize: # 캐시가 꽉 찼을 때
             if i in cache:
                 answer += 1
                 idx = cache.index(i)
