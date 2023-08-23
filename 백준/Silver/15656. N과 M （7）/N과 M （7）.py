95424f45741dbd11b56b6e8c8a6437234d2107e1
Seungwon <63894186+dnjs5397@users.noreply.github.com>
2023-08-23 15:57:09 +0900

    [Silver III] Title: N과 M (7), Time: 2952 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+answer = []
+
+
+def dfs(v):
+    if len(answer) == m:
+        for i in answer:
+            print(arr[i], end=' ')
+        print()
+        return
+
+    for i in range(n):
+        answer.append(i)
+        dfs(i)
+        answer.pop()
+
+
+dfs(0)
